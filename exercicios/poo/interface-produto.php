e206e9b7dcc9ed497d21e579930d6aaec701b6b9
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-10-28 14:19:10 -0300

    refactor: organizando melhor o código

---
+++ exercicios/poo/interface-produto.php
@@ -0,0 +1,6 @@
+<?php
+
+// Interface Produto:
+
+// Crie uma interface Produto com o método calcularPreco(). Em seguida, desenvolva duas classes que implementam essa interface: Livro e Eletronico. Cada classe deve ter seu próprio método para calcular o preço (com ou sem desconto, por exemplo).
+// Dica: Utilize a palavra-chave implements para garantir que as classes implementem a interface.
